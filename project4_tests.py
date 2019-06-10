30e4f2ede64991d4a244f6d53ec35ad0adaa96fc
Christopher Specht <cfspecht@users.noreply.github.com>
2019-06-10 00:50:06 -0700

    Create project4_tests.py

---
+++ project4_tests.py
@@ -0,0 +1,21 @@
+""" Project 4: Document Search Engine Test Suite
+Chris Specht
+CPE 202-09
+Spring 2019
+"""
+
+
+import unittest
+from project4 import *
+
+
+class TestCase:
+    pass
+
+
+def test_main():
+    unittest.main()
+
+
+if __name__ == "__main__":
+    test_main()
